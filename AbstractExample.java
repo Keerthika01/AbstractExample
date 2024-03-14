3e6d730ce9afbc3289d87de533c3b9999d9a2e1f
Keerthika01 <94049105+Keerthika01@users.noreply.github.com>
2024-03-14 20:45:51 +0530

    Add files via upload

---
+++ AbstractExample.java
@@ -0,0 +1,41 @@
+package com.sessions;
+
+abstract class Addition
+{
+	abstract void add();
+	abstract void divi();
+	
+	public void sub()
+	{
+		System.out.println("Subtraction");
+	}
+	
+	public void mul()
+	{
+		System.out.println("Multiplication");
+	}
+}
+class ImplementMethod extends Addition
+{
+	void add()
+	{
+		System.out.println("Addition");
+	}
+	
+	void divi()
+	{
+		System.out.println("Division");
+	}
+}
+public class AbstractExample {
+
+	public static void main(String[] args)
+	{
+		ImplementMethod obj= new ImplementMethod();
+		obj.add();
+		obj.sub();
+		obj.mul();
+		obj.divi();
+	}
+
+}
